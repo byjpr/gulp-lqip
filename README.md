e63edf9311ba6df20dffc7aa374687ced26c3f1a
Jordan Parker <letsgetjordan@gmail.com>
2017-06-12 20:58:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center">
+  <img src="https://lqip-loader.firebaseapp.com/media/logo.png" width="200">
+    <br />
+    LQIP-LOADER for GULP
+    <br />
+</h1>
+
+<h4 align="center">LQIP: Low Quality Images Placeholder</h4>
+
+#### Inspired By:
+- lqip for webpack
+- gulp inline image
+- more
---
+++ index.js
@@ -0,0 +1,74 @@
+var cheerio = require('cheerio')
+var path = require('path')
+var fs = require('fs')
+var url = require('url')
+var sharp = require('sharp')
+
+var gutil = require('gulp-util')
+var PluginError = gutil.PluginError
+
+var contentTypes = {
+  ".png": "image/png",
+  ".gif": "image/gif",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".bmp": "image/bmp",
+  ".webp": "image/webp"
+}
+
+var toBase64 = function (extension, data) {
+  return 'data:' + contentTypes[extension] + ';base64,' + data.toString('base64');
+};
+
+
+function inliner(html, base) {
+  base = base || process.cwd()
+
+  var dom = cheerio.load(String(html))
+  inlineImages(dom)
+
+  return new Buffer(dom.html({decodeEntities: false}))
+
+  function inlineImages(dom) {
+    var styles = [];
+    dom('img').each(function(idx, el) {
+      el = dom(el)
+      var src = el.attr('src')
+      if (src && isLocal(src)) {
+        var dir = path.dirname(src)
+        var path = path.join(base, src)
+        sharp(path)
+          .resize(14) // resize to 16px width and auto height
+          .toBuffer() // converts to buffer for Base64 conversion
+          .then(data => {
+            presource = toBase64(extension, data)
+            el.attr('src', presource)
+            el.attr('data-CogitipLoad-src', src)
+          })
+      }
+    })
+  }
+
+  function isLocal(href) {
+    return href && !url.parse(href).hostname;
+  }
+}
+
+function gulpExport(dir) {
+  var stream = through.obj(function(file, enc, cb) {
+    if (file.isStream()) {
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+      return cb();
+    }
+    if (file.isBuffer()) {
+      file.contents = inliner(file.contents, dir);
+      this.push(file);
+      return cb();
+    }
+    return cb(null, file); //no-op
+  });
+
+  return stream;
+}
+
+module.exports = gulpExport;
